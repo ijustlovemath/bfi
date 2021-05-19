798febe913828d431760e4661845892d193d82b6
Jeremy DeJournett <jcdejournett@gmail.com>
2021-05-19 13:26:37 -0400

    Added bound checks for data pointer, some new comments and allowed run() to take a string literal.

--- bfi.c
+++ bfi.c
@@ -9,7 +9,14 @@ void die(const char *msg)
 	int x = *(int *)0;
 }
 
-void do_fsm(const char *prgm, unsigned char **dp, unsigned char **pc)
+/**
+ * dp is a pointer to the data pointer; that way we can manipulate the data
+ * pointer itself, and not just the data it points to. This allows us to
+ * easily implement both '>' and '+' instructions.
+ * pc is similar, it's a pointer to the program counter, which we use to index
+ * into the program text itself.
+ */
+void do_fsm(unsigned char **dp, const char **pc)
 {
 #define print_byte(b_) do {\
   fprintf(stderr, #b_ ": '%c', %02x\n", b_, b_);\
@@ -20,6 +27,9 @@ void do_fsm(const char *prgm, unsigned char **dp, unsigned char **pc)
  * branch character, accounting for nested loops.
  * needs to have a check for overrunning past program text, in case of
  * unmatched braces
+ * This is not quite as efficient as doing a single pass to note down
+ * locations of all open braces and their matching closing brace, but
+ * it's quick to write and this interpreter is meant to be simple, not fast.
  */
 #define BRANCH_CASE(branch_char, matched_char, direction, dp_negation) \
     case branch_char:\
@@ -76,31 +86,34 @@ void cpy(unsigned char *dst, unsigned char *src, unsigned int len)
 #undef dbg
 #define dbg(...)
 
-void run(unsigned char *prgm)
+#define BOUNDED_BY(start, ptr, end) ((ptr >= start) && (ptr < end))
+
+void run(const char *prgm)
 {
 	unsigned char data[WORKSPACE_SIZE] = {0};
 	unsigned char *dp = data;
-	unsigned char *pc = prgm;
+	const char *pc = prgm;
 
 	dbg(pc, dp, prgm);
-	while(pc >= prgm && pc < prgm + WORKSPACE_SIZE && *pc) {
-		do_fsm(prgm, &dp, &pc);
+	while(BOUNDED_BY(prgm, pc, prgm + WORKSPACE_SIZE) && BOUNDED_BY(data, dp, data + WORKSPACE_SIZE) && *pc) {
+		do_fsm(&dp, &pc);
 		dbg(pc, dp, prgm);
 		pc++;
 	}
 }
 
-void xcpy(unsigned char *dest, const char *src)
+void xcpy(char *dest, const char *src)
 {
     unsigned int len = 0;
     for(; len < WORKSPACE_SIZE && *src; *dest = *src, src++, dest++, len++);
+    // zero out the rest to prevent multiple programs from cluttering the program buffer
+    for(; len < WORKSPACE_SIZE; *dest = 0, dest++, len++);
 }
 
 int main(void)
 {
-	unsigned char prgm [WORKSPACE_SIZE] = {0};
+	char prgm [WORKSPACE_SIZE] = {0};
 
-    
 	// Hello, World!
     xcpy(prgm, ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.");
 
@@ -129,6 +142,9 @@ int main(void)
     //xcpy(prgm, "++++++++[>+>++++<<-]>++>>+<[-[>>+<<-]+>>]>+[-<<<[->[+[-]+>++>>>-<<]<[<]>>++++++[<<+++++>>-]+<<++.[-]<<]>.>+[>>]>+]");
 
 	//xcpy(prgm, ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.");
-    run(prgm);
+    //run(prgm);
+
+    // For now, just run a simple "Hello, World!" program every time.
+	run(">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.");
     return 0;
 }
